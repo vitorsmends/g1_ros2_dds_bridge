451d8072aa750bd88e25e85b0ea12092ac506301
mendes <vitorsmendscod@gmail.com>
2026-02-24 18:42:17 -0300

    add nodes and ros2 setup files

---
+++ launch/dds_bridge.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="g1_ros2_dds_bridge",
+            executable="lowstate_to_dog_odom.py",
+            name="lowstate_to_dog_odom",
+            output="screen",
+            parameters=[{
+                "dds_domain_id": 0,
+                "dds_topic": "rt/lowstate",
+                "ros_topic": "/dog_odom",
+                "odom_frame": "odom",
+                "base_frame": "base_link",
+            }],
+        ),
+        Node(
+            package="g1_ros2_dds_bridge",
+            executable="livox_to_pointcloud2.py",
+            name="livox_to_pointcloud2",
+            output="screen",
+            parameters=[{
+                "dds_domain_id": 0,
+                "dds_topic": "rt/utlidar/cloud_livox_mid360",
+                "ros_topic": "/livox/lidar",
+                "override_frame_id": "",
+            }],
+        ),
+    ])
--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-package_name = 'g1_ros2_dds_bridge'
+package_name = "g1_ros2_dds_bridge"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.1",
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", ["launch/dds_bridge.launch.py"]),
+        ("share/" + package_name + "/scripts", ["scripts/lowstate_to_dog_odom.py", "scripts/livox_to_pointcloud2.py"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='dev',
-    maintainer_email='vitormendesrb@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer="Vitor Mendes",
+    maintainer_email="vitormendesrb@gmail.com",
+    description="DDS to ROS 2 bridge for Unitree G1 (LowState odom + Livox PointCloud2)",
+    license="Apache-2.0",
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "lowstate_to_dog_odom = g1_ros2_dds_bridge.lowstate_to_dog_odom:main",
+            "livox_to_pointcloud2 = g1_ros2_dds_bridge.livox_to_pointcloud2:main",
         ],
     },
 )
