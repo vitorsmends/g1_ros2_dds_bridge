5f793e57dc4f66ce145136681e5c4343ad0be85f
mendes <vitorsmendscod@gmail.com>
2026-02-24 18:53:01 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", ["launch/dds_bridge.launch.py"]),
-        ("share/" + package_name + "/scripts", ["scripts/lowstate_to_dog_odom.py", "scripts/livox_to_pointcloud2.py"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
