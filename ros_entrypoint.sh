5777a2f21c1c577f61861fb227d96b09151de4c6
mendes <vitorsmendscod@gmail.com>
2026-03-02 14:47:03 -0300

    comment launch entrypoint for tests

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source /opt/ros/humble/setup.bash
 source /opt/ws/install/setup.bash
 
-exec ros2 launch g1_ros2_dds_bridge dds_bridge.launch.py
+# exec ros2 launch g1_ros2_dds_bridge dds_bridge.launch.py
