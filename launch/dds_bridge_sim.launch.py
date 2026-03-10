208ac9148ffb4f000c5e96e9326db7c6756a2075
mendes <vitorsmendscod@gmail.com>
2026-03-10 15:12:17 -0300

    add launch for simulation

--- launch/dds_bridge_sim.launch.py
+++ launch/dds_bridge_sim.launch.py
@@ -12,9 +12,21 @@ def generate_launch_description():
             parameters=[{
                 "dds_domain_id": 0,
                 "dds_topic": "rt/lowstate",
-                "ros_topic": "/dog_odom_fixed",
+                "ros_topic": "/dog_odom",
                 "odom_frame": "odom",
                 "base_frame": "base_link",
+            }],
+        ),
+        Node(
+            package="g1_ros2_dds_bridge",
+            executable="livox_to_pointcloud2",
+            name="livox_to_pointcloud2",
+            output="screen",
+            parameters=[{
+                "dds_domain_id": 0,
+                "dds_topic": "rt/utlidar/cloud_livox_mid360",
+                "ros_topic": "/livox/lidar",
+                "override_frame_id": "",
             }],
         ),
     ])
